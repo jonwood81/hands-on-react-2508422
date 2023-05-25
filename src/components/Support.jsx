dcc6dfeeea6ad384b01fd33afe52c48fe4ee061d
jonwood81 <81443035+jonwood81@users.noreply.github.com>
2023-05-25 23:18:11 +0000

    added changes for useState and useEffect

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -1,12 +1,18 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+const pageTitle = document.title;
 
 export default () => {
   //This is declaring const variable in memory and methods (count, setCount) to ... 
   //...modify it (an array in this case),  and initializing with the '0' value.
   const [count, setCount] = useState(0)
+  
+  useEffect(() => {
+    if (count > 0) {
+      document.title = `${pageTitle}--${count}`;
+    }
+  })
   return (
     <button className="outline" onClick={() => setCount(count +1)}>
-
       {(()=>{
         if (count === 0){
           return "click to support";
@@ -15,5 +21,8 @@ export default () => {
         }
       })()}
     </button>
+    // Using ternary statement ...
+    //{count === 0? "click to support": `Supported ${count} times`}
+    //...the same can be accomplished with a shorter format 
   )
 }
