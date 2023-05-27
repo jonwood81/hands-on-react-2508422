8fd4772f38844855c87c7811a26232d9cf0681a1
jonwood81 <81443035+jonwood81@users.noreply.github.com>
2023-05-27 00:41:35 +0000

    Beginning of modal class

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,35 @@
-import React, { useEffect, useState } from 'react';
+import React, { Component, useState } from 'react';
 import ListCast from './components/ListCast';
 import Modals from './components/Modals';
 import Nav from './components/Nav';
-import Welcome from './components/Welcome';
-import Support from './components/Support';
 import "./App.scss";
 
 function App() {
   const name = 'StarGazers'
+  let [memberInfo, setMemberInfo] = useState(null); 
   return (
     <div className="container">
-      <article>
         <hgroup>
           <img src="images/group.svg" alt="StarGazers Group" />
-          
-          <Welcome name="StarGazers"/>
-          
-          <p>Members of an <b>intergalactic alliance</b><br />
-            paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-          <Support />
+          <h1>Meet the StarGazers</h1>
+          <p>Members of an <b>intergalactic alliance</b>paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
         </hgroup>
-      </article>
+        <ListCast onChoice={ (info) => { setMemberInfo(info)}} />
+        { memberInfo && 
+          <article>
+            <hgroup>
+              <div style={{
+                display: "flex",
+                gap: '1rem'
+              }}>
+                <img style={{ width: "200px"}} src={`images/${memberInfo.slug}.svg`} alt={memberInfo.name} />
+
+                <h1>{memberInfo.name}</h1>
+                <p>{memberInfo.bio}</p>
+              </div>
+            </hgroup>
+          </article>
+        }
     </div>
   )
 }
--- src/components/ListCast.jsx
+++ src/components/ListCast.jsx
@@ -1,3 +1,42 @@
+import {useState, useEffect} from "react";
+
+//pass member info along using the "onChoice" event.  This is triggered by the "onChoice" ...
+//..in the 'export default' line below and in the "onClick" within the <a> tag/element below...
+export default ({onChoice}) => {
+  const [cast, setCast] = useState([]);
+
+  async function fetchCast() {
+    const response = await fetch('cast.json');
+    setCast(await response.json());
+  }
+
+  //Use the "useEffect" to load elements after page has rendered.  In this case ...
+  //...we are using it in combination with the "useState" hook.
+useEffect(() => {
+  fetchCast();
+});
+    //Show the cast images (cartoon aliens) in a grid format.  Also add a tooltip ...
+    //...that displays the name of the member when the user hovers over an image. 
+    //This uses the "member id" that each member has in "cast.json" 
+		return (
+		  <div style={{
+		    display: "grid",
+		    gridTemplateColumns: `repeat(auto-fit, minmax(90px, 1fr))`,
+		    gap: `1rem`,
+		    marginBottom: `1rem`
+		  }}>
+		    {
+		      cast.map(member => (
+		        <a onClick={ () => {onChoice(member)}} key={member.id} data-tooltip={member.name}> 
+		        <img src={`images/${member.slug}_tn.svg`} alt={member.name} />
+		        </a>
+		      ))
+		    }
+		  </div>
+		)
+
+}
+/*
 export default ({ cast, onChoice }) => {
   return (
     <div style={{
@@ -16,3 +55,4 @@ export default ({ cast, onChoice }) => {
     </div>
   )
 }
+*/
